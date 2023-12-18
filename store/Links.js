db398f48547d366bca924a9282ee402564246bdb
Cory Taylor <official.cstdesign@gmail.com>
2023-12-18 17:28:55 -0600

    removed spaces from How To Use href in Links.js

--- store/Links.js
+++ store/Links.js
@@ -1,6 +1,6 @@
 export default [
   { title: "Home", text: "Home" },
-  { title: "How To Use", text: "How To Use" },
+  { title: "HowToUse", text: "How To Use" },
   { title: "Recipes", text: "Recipes" },
   { title: "About", text: "About" },
   { title: "Contact", text: "Contact" }
